22164eeaee8b2fa5d48bfde0ae26f5c560f05595
Chris H <122721974+CH3RNAND3Z@users.noreply.github.com>
2023-02-26 22:12:21 -0800

    fine tuning

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+const timeCount = document.querySelector('#timer #timer_sec');
 
 let currentQuestion = {}
 let acceptingAnswers = true
@@ -12,48 +13,59 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question:
-            "The tallest building in the world is located in which city?",
-        choice1: "Dubai",
-        choice2: "New York",
-        choice3: "Shanghai",
-        choice4: "None of the above",
-        answer: 1,
-    },
-    {
-        question: "What percent of American adults believe that chocolate milk comes from brown cows?",
-        choice1: "20%",
-        choice2: "18%",
-        choice3: "7%",
-        choice4: "33%",
+        question: "Commonly used data types DO Not Include:",
+        choice1: "Strings",
+        choice2: "Booleans",
+        choice3: "Alerts",
+        choice4: "Numbers",
         answer: 3,
     },
     {
-        question: "Approximately what percent of U.S. power outages are caused by squirrels?",
-        choice1: "10-20%",
-        choice2: "5-10%",
-        choice3: "15-20%",
-        choice4: "30%-40%",
-        answer: 1,
+        question:
+            "The condition in an if / else statement is enclosed with ____.",
+        choice1: "Quotes",
+        choice2: "Curly brackets",
+        choice3: "Parenthesis",
+        choice4: "Square brackets",
+        answer: 3,
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        choice1: "Numbers and strings",
+        choice2: "Other arrays",
+        choice3: "Booleans",
+        choice4: "All of the above",
+        answer: 4,
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choice1: "Commas",
+        choice2: "Curly brackets",
+        choice3: "Quotes",
+        choice4: "Parenthesis",
+        answer: 3,
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choice1: "JavaScript",
+        choice2: "Terminal/Bash",
+        choice3: "For loops",
+        choice4: "Concole.log",
+        answer: 4,
     }
 ]
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 20
+const MAX_QUESTIONS = 5
+
 
 startGame = () => {
     questionCounter = 0
     score = 0
     availableQuestions = [...questions]
     getNewQuestion()
+    startTimer(60)
+    /*let timeValue = 60;*/
 }
 
 getNewQuestion = () => {
@@ -79,6 +91,24 @@ getNewQuestion = () => {
     availableQuestions.splice(questionsIndex, 1)
 
     acceptingAnswers = true
+    clearInterval(counter);
+    startTimer(60);
+}
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timeCount.textContent = time;
+        time--;
+        if(time < 0) {
+            clearInterval(counter);
+            timeCount.textContent = '0';
+        }
+    }
+
+    if(time === 0){};
+    then(stop)
+
 }
 
 choices.forEach(choice => {
@@ -101,13 +131,19 @@ choices.forEach(choice => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
 
+            
+        
         }, 1000)
     })
 })
+
+let counter;
+
+
 
 incrementScore = num => {
     score +=num
     scoreText.innerText = score
 }
-
+/*startTimer(60)*/
 startGame()
